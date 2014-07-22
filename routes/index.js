d1c9d96a526a069ccc8bf0adc3e1e79b0199fe7a
formellaalex <formella.alex@gmail.com>
2014-07-22 15:46:55 +0200

    Wywalona baza, dodany licznik eventow

--- routes/index.js
+++ routes/index.js
@@ -3,47 +3,17 @@ var router = express.Router();
 
 
 /* GET home page. */
-/*router.get('/', function(req, res) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express'});
 });
-*/
-router.get('/', function(req, res) {
+
+router.get('/bazatest', function(req, res) {
     var db = req.db;
     var collection = db.get('userlist');
     collection.find({},{},function(e,docs){
-        res.render('index', {
-            "index" : docs
+        res.render('bazatest', {
+            "bazatest" : docs
         });
-    });
-});
-
-router.post('/adduser', function(req, res) {
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-
-    // Set our collection
-    var collection = db.get('userlist');
-
-    // Submit to the DB
-    collection.insert({
-        "imie" : userName,
-        "nazwisko" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("/");
-            // And forward to success page
-            res.redirect("/");
-        }
     });
 });
 module.exports = router;
